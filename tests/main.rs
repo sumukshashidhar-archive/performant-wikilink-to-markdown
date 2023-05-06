e7cd105caa122baa78049d71f7ae7c679e4995e6
Sumuk Shashidhar <sumuks2@illinois.edu>
2023-05-06 11:25:26 -0500

    tests: add slightly better cargo test for filename

--- tests/main.rs
+++ tests/main.rs
@@ -1,4 +1,7 @@
 use std::process::Command;
+use std::collections::HashSet;
+use walkdir::WalkDir;
+
 
 #[test]
 fn test_program() {
@@ -10,4 +13,36 @@ fn test_program() {
         .expect("failed to run program");
 
     assert!(output.status.success(), "program exited with an error: {:?}", output);
+
+    let ideal_dir = "./tests/ideal";
+    let output_dir = "./tests/output";
+
+    // Collect the filenames in ideal directory
+    let ideal_filenames: HashSet<String> = WalkDir::new(ideal_dir)
+        .into_iter()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            if entry.file_type().is_file() {
+                Some(entry.file_name().to_string_lossy().to_string())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    // Collect the filenames in output directory
+    let output_filenames: HashSet<String> = WalkDir::new(output_dir)
+        .into_iter()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            if entry.file_type().is_file() {
+                Some(entry.file_name().to_string_lossy().to_string())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    // Check if the filename sets are equal
+    assert_eq!(ideal_filenames, output_filenames, "The output directory does not match the ideal directory");
 }
