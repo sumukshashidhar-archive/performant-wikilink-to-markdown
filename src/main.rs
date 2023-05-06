4533ed4b3e0a4fe04092f76fcc50711f9387e99e
Sumuk Shashidhar <sumuks2@illinois.edu>
2023-05-06 08:55:02 -0500

    feat: add directory listing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-use std::fs::File;
-use std::io::{BufReader, Read};
+use std::fs;
 
 fn main() {
-    match read_file("data/a.md") {
-        Ok(contents) => println!("{}", contents),
-        Err(error) => eprintln!("Error reading file: {}", error),
+    let path = "data";
+    let entries = get_entries(path);
+    for entry in entries {
+        println!("{}", entry);
     }
 }
 
-fn read_file(path: &str) -> Result<String, std::io::Error> {
-    let file = File::open(path)?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
-    Ok(contents)
+fn get_entries(path: &str) -> Vec<String> {
+    let mut entries = Vec::new();
+    if let Ok(read_dir) = fs::read_dir(path) {
+        for entry_result in read_dir {
+            if let Ok(entry) = entry_result {
+                let entry_path = entry.path();
+                if entry_path.is_dir() {
+                    entries.push(entry_path.to_str().unwrap().to_owned());
+                    let subentries = get_entries(entry_path.to_str().unwrap());
+                    entries.extend(subentries);
+                } else if entry_path.is_file() {
+                    entries.push(entry_path.to_str().unwrap().to_owned());
+                }
+            }
+        }
+    }
+    entries
 }
